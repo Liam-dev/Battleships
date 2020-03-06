6da676795509e91398bd15aa36c11935c9424e1e
Liam-dev <liam2159@gmail.com>
2020-03-06 10:01:14 +0000

    game class started

--- Battleships/Game.cs
+++ Battleships/Game.cs
@@ -8,6 +8,19 @@ namespace Battleships
 {
     class Game
     {
+        protected List<Player> players;
 
+        public Game(List<Player> players)
+        {
+            this.players = players;
+
+            while (players.FindAll(player => player.HasLost).Count == 0)
+            {
+                foreach (Player player in players)
+                {
+                    player.Turn();
+                }
+            }
+        }
     }
 }
