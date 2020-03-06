a5a964559fd5f20768ecc05c39a87a224c4899a3
Liam-dev <liam2159@gmail.com>
2020-03-06 13:32:16 +0000

    game working

--- Battleships/Game.cs
+++ Battleships/Game.cs
@@ -9,10 +9,24 @@ namespace Battleships
     class Game
     {
         protected List<Player> players;
+        Random rnd;
 
-        public Game(List<Player> players)
+        public Game(List<Player> players, Random rnd)
         {
+            this.rnd = rnd;
+
             this.players = players;
+            players[0].SetOpponent(players[1]);
+            players[1].SetOpponent(players[0]);
+
+            foreach (Player player in players)
+            {
+                player.PlaceShips(rnd, false);
+            }
+        }
+
+        public void Play()
+        {
 
             while (players.FindAll(player => player.HasLost).Count == 0)
             {
--- Battleships/Program.cs
+++ Battleships/Program.cs
@@ -13,59 +13,14 @@ namespace Battleships
             Console.Title = "Battleships";
             Random rnd = new Random();
 
-            User liam = new User("Liam");
-            Opponent oppo = new Opponent(rnd);
-
-            liam.SetOpponent(oppo);
-            oppo.SetOpponent(liam);
-
-            liam.PlaceShips(rnd, true);
-            oppo.PlaceShips(rnd, true);
-           
-            for (int i = 0; i < 100; i++)
+            List<Player> players = new List<Player>()
             {
-                oppo.Turn();
-                liam.Turn();
-            }
+                new User("Liam"),
+                new Opponent(rnd)
+            };
 
-            //liam.FireShot(new Position(1, 1));
-            //liam.FireShot(new Position(8, 1));
-            
-
-            liam.PrintBoards();
-
-
-
-
-            /*
-            
-            Console.WriteLine("\nLiam:\n");
-            liam.ownBoard.Print(new Position(0, 3));
-
-            Console.CursorLeft = 75;
-            Console.CursorTop = 1;
-            Console.Write("Opponent:\n");
-            oppo.ownBoard.Print(new Position(75, 3));
-            
-            
-
-            Console.WriteLine("\n\n");
-            for (int i = 0; i < 10; i++)
-            {
-                oppo.FireShot(new Position(9 - i, 9 - i));
-
-            }
-
-            Console.Write("Liam:\n");
-            liam.ownBoard.Update();
-            liam.ownBoard.Print(new Position(0, 30));
-
-            Console.CursorLeft = 75;
-            Console.CursorTop = 28;
-            Console.Write("Opponent:\n");
-            oppo.ownBoard.Update();
-            oppo.ownBoard.Print(new Position(75, 30));
-            */
+            Game newGame = new Game(players, rnd);
+            newGame.Play();
         }
     }
 }
